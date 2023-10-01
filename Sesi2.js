7ece103988282f2e191897e18b40d70079fe83e9
Maulana Ibrahim <120733899+ItsGBoys@users.noreply.github.com>
2023-10-01 09:02:25 +0700

    Create Sesi2.js

---
+++ Sesi2.js
@@ -0,0 +1,9 @@
+let firstName = "Maulana ";
+let lastName = "Ibrahim ";
+let age = 20;
+let isMarried = false;
+
+console.log(" Nama depan : "+ firstName)
+console.log(" Nama belakang : "+ lastName)
+console.log(" Usia : "+ age)
+console.log(" Status Perkawinan : "+ isMarried)
